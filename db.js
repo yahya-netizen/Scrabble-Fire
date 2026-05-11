2227e5184e084c20e2fd9a0a1f5839142f1fa696
farid <ignrid0@gmail.com>
2026-05-11 22:33:04 +0700

    add_DB_PORT to mysql connection

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT) || 3306,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'uas_susun_kata',
